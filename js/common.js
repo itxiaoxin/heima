901c9973c4e319a246481669c25688d6aa550917
xiaoxin <738665801@qq.com>
2019-04-06 16:11:09 +0800

    第9次提交，首页全部完成，接下来做category

--- js/common.js
+++ js/common.js
@@ -1,10 +1,14 @@
-$(function(){
+$(function () {
+    // 解决移动端点击a标签不能跳转的问题，这是mui框架的默认设置，需要手动设置tap事件实现跳转
+    mui('body').on('tap', 'a',function () {
+            document.location.href = this.href;
+        })
     // 定义基准路径
-    const baseURL='http://157.122.54.189:9094/api/public/v1/';
+    const baseURL = 'http://157.122.54.189:9094/api/public/v1/';
     // 添加zepto拦截器，它的作用是让每个ajax请求都经过这个函数进行处理
-    $.ajaxSettings.beforeSend=function(xhr,obj){
+    $.ajaxSettings.beforeSend = function (xhr, obj) {
         // console.log(obj);
         // 拼接路径并重新赋值 obj.url就是发送ajax请求时的url
-        obj.url=baseURL+obj.url;
+        obj.url = baseURL + obj.url;
     }
 })
