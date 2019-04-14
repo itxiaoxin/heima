7f2e4f74c55f362f66b8529451c167c587da53d0
xiaoxin <738665801@qq.com>
2019-04-14 23:13:44 +0800

    第21次提交，商品详情动态渲染完成，接下来做立即购买及假如购物车业务

--- js/goodDetail.js
+++ js/goodDetail.js
@@ -2,26 +2,25 @@ $(function () {
     // 区域滚动初始化
     mui('.mui-scroll-wrapper').scroll({
         deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
-        indicators:false    //设置没有滚动条，默认值是ture
-    });
-
-    //mui-图片轮播初始化
-    var gallery = mui('.mui-slider');
-    gallery.slider({
-        interval: 2000//自动轮播周期，若为0则不自动播放，默认为0；
+        indicators: false    //设置没有滚动条，默认值是ture
     });
 
 
     // 发送ajax请求获取数据，渲染页面
     $.ajax({
-        type:'get',
-        url:'goods/detail',
-        data:$.getParam(location.search),
-        dataType:'json',
-        success:function(result){
-            console.log(result);
-            var html=template('goodsDetailTemp',result.data);
-            $('.goodsDescription').html(html);
+        type: 'get',
+        url: 'goods/detail',
+        data: $.getParam(location.search),
+        dataType: 'json',
+        success: function (result) {
+            // console.log(result);
+            var html = template('goodTemp', result.data);
+            $('.goodsInfo').html(html);
+            //mui-图片轮播初始化
+            var gallery = mui('.mui-slider');
+            gallery.slider({
+                interval: 2000//自动轮播周期，若为0则不自动播放，默认为0；
+            });
         }
     })
 })
