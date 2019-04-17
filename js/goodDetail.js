923f824029dd94c638255b102c63dd586fdd456b
xiaoxin <738665801@qq.com>
2019-04-17 20:55:04 +0800

    商品详情页立即购买发送请求部分完成，接下来做登录部分，根据token判断是否登录

--- js/goodDetail.js
+++ js/goodDetail.js
@@ -5,8 +5,19 @@ $(function () {
         indicators: false    //设置没有滚动条，默认值是ture
     });
 
+// 这是加入购物车时需要提交的数据，因为需要从商品详情数据中获取，所以创建一个全局变量
+    var carInfo={
+        cat_id:'',
+        goods_id:'',
+        goods_name:'',
+        goods_number:'',
+        goods_price:'',
+        goods_small_logo:'',
+        goods_weight:''
 
-    // 发送ajax请求获取数据，渲染页面
+    }
+
+    // 发送ajax请求获取商品详情数据，渲染页面
     $.ajax({
         type: 'get',
         url: 'goods/detail',
@@ -14,6 +25,15 @@ $(function () {
         dataType: 'json',
         success: function (result) {
             // console.log(result);
+            // 以下信息是要发送加入购物车请求时需要提交的数据
+            carInfo.cat_id=result.data.cat_id;
+            carInfo.goods_id=result.data.goods_id;
+            carInfo.goods_name=result.data.goods_name;
+            carInfo.goods_number=result.data.goods_number;
+            carInfo.goods_price=result.data.goods_price;
+            carInfo.goods_small_logo=result.data.goods_small_logo;
+            carInfo.goods_weight=result.data.goods_weight;
+
             var html = template('goodTemp', result.data);
             $('.goodsInfo').html(html);
             //mui-图片轮播初始化
@@ -22,5 +42,14 @@ $(function () {
                 interval: 2000//自动轮播周期，若为0则不自动播放，默认为0；
             });
         }
+    })
+
+    // 加入购物车
+    $('.addCart').on('tap',function(){
+        // 点击加入购物车，发送请求
+        $.post('my/cart/add',carInfo,function(result){
+            console.log(result);
+            
+        },'json')
     })
 })
