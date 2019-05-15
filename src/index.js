e2997a5a6c15701911244a746784924ce2ebb143
xiaoxin <738665801@qq.com>
2019-05-15 19:02:24 +0800

    第1次提交，项目初始化

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+// 引入fastclick,消除移动端的点击事件延迟
+import FastClick from 'fastclick';
+FastClick.attach(document.body);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
